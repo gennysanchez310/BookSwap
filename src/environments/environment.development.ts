23839457b59296da1320522f5c9b791822f4edb9
JACM701 <josuechucmedina980@gmail.com>
2025-02-24 14:03:28 -0600

    gitignore cambiado y quitado lo de enrolloment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCevKce78f1CrhbZ6sF-gp-JVNXnO5Log",
+    authDomain: "bookswap-web.firebaseapp.com",
+    projectId: "bookswap-web",
+    storageBucket: "bookswap-web.firebasestorage.app",
+    messagingSenderId: "310107147673",
+    appId: "1:310107147673:web:8f69f463dc00c0f4ce1708"
+  },
+  apiEndpoints: {
+    googleBooks: 'https://www.googleapis.com/books/v1/volumes?q=isbn=',
+    openStreetMap: 'https://nominatim.openstreetmap.org/search?format=json&q=',
+    dicebearAvatar: 'https://api.dicebear.com/7.x/micah/svg?seed='
+  }
+};
