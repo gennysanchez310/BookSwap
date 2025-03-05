cf67b3f28e16e03796ce0340a70fddc53930c256
JACM701 <josuechucmedina980@gmail.com>
2025-03-04 19:27:34 -0600

    V1 Mobil incluido

---
+++ android/app/src/main/java/com/BookSwapMovil/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.BookSwapMovil.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.BookSwapMovil.app',
+  appName: 'book-swap',
+  webDir: 'dist'
+};
+
+export default config;
